6c9f22f64c07815f03a23bbf092f23d0649fe185
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-06 08:00:21 -0700

    added a note

--- index.ts
+++ index.ts
@@ -23,3 +23,7 @@ else{
     console.log("Oops❗ you Guessed wrong number🤦");
     
 }
+//FOR THE TEACHER :
+// this project is only made by arisha Ghaffar.
+//me and my sister both uses the same laptop thats why 
+//whenever we upload our projects on github it shows that this is made by both of us.
